14147ef5421fe780682ccc28b4dfa1a8a2e4f701
lujian <lujian01@hujiang.com>
2021-10-15 15:05:39 +0800

    update

---
+++ src/ui/components/editable_text.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {Input, Typography, Space} from 'antd';
+import { EDIT_ICON } from '@/ui/constants/icons'
+import RequiredInput from './required_input'
+import {stopClickPropagation} from '@/utils/global_utils';
+import 'ui/style/required_input.css'
+import Ellipsis from 'react-ellipsis-component';
+const {Text} = Typography;
+
+class EditableText extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.defaultValue,
+            editing: props.editing,
+        }
+    }
+
+    componentDidMount() {
+      
+    }
+
+    handleEditIconClick = (e) => {
+        this.setState({editing: true});
+        stopClickPropagation(e)
+    }
+
+    handleSave = (value) => {
+        this.setState({editing: false});
+        this.props.onSave(value)
+    }
+
+    handleChange = (value) => {
+        this.props.onChange(value)
+    }
+
+    render() {
+     
+        const {editIconClass, editWhenHover, value, ellipisisWidth} = this.props;
+        
+        const {editing} = this.state;
+
+        return (
+            <>
+                {            
+                    editing ? (
+                        <RequiredInput 
+                            value={value} 
+                            onSave={this.handleSave} 
+                            onChange={this.handleChange}
+                        />
+                    ) : (
+                        // <Space className={editWhenHover ? "not-edit-container" : ""} >
+                        //     <span className={editWhenHover ? "not-editing-text" : ""} style={{border: '1px solid rgb(0,0,0,1)', display: 'inline-block'}}>
+                        //         {value}
+                        //     </span>
+                        //     <span className={'not-editing-edit-icon ' + (editIcon.className ? editIcon.className : "")} onClick={this.handleEditIconClick}>{EDIT_ICON}</span>
+                        // </Space>
+                        <Space>
+                            <Ellipsis
+                                text={value}
+                                maxLine="1"
+                            />
+                            <span className={editIconClass} onClick={this.handleEditIconClick}>{EDIT_ICON}</span>
+                        </Space>
+                    )
+                }
+            
+            </>
+           
+            
+        )
+    }
+}
+
+export default EditableText;
+EditableText.defaultProps = {
+    onChange: () => {},
+    onSave: () => {},
+}
+
+
+
+
+
+
+
