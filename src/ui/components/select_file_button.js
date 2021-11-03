a1bdf8cc64ecb622514c7d4454d1ff30a863a8ac
lujian <lujian01@hujiang.com>
2021-11-03 14:33:14 +0800

    body formdata

---
+++ src/ui/components/select_file_button.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Tooltip, Button, Typography} from 'antd';
+
+const {Link} = Typography;
+class SelectFileButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+      
+    }
+
+    handleSelectBtnClick = () => {
+        const openFileSelect = window.require('@electron/remote').getGlobal('OPEN_FILES_ELECT_DIALOG');
+        let selectedFiles = openFileSelect();
+        this.props.onSelect(selectedFiles || [])
+    }
+
+    render() {
+        const {buttonProps = {}, defaultLabel = 'Select Files', value = []} = this.props;
+        let buttonLabel = defaultLabel;
+        if (value.length === 1) {
+            let firstFilePath = value[0];
+            buttonLabel = firstFilePath.substr(firstFilePath.lastIndexOf(window.require('path').sep) + 1)
+        } else if (value.length > 1) {
+            buttonLabel = value.length + " files selected";
+        }
+        return (
+            <Tooltip title={value.length > 0 ? value.join(',\n') : 'No file selected'}>
+                <Button size="small" {...buttonProps} onClick={this.handleSelectBtnClick}>
+                    {buttonLabel}
+                </Button>
+            </Tooltip>
+            
+        )
+    }
+}
+
+export default SelectFileButton;
+
+SelectFileButton.defaultProps = {
+    onSelect: () => {},
+}
+
+
+
+
+
+
+
